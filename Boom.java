0876ef7c2f186e6e722e9a73d831857951c3cc73
Kumex21 <115811447+Kumex21@users.noreply.github.com>
2022-11-14 15:28:42 +0200

    Add files via upload

---
+++ Boom.java
@@ -0,0 +1,11 @@
+public class Boom {
+    public static void main(String[] args) {
+
+        for (int a = 5; a > 0; a--) {
+            System.out.println("Time: " + a);
+            if (a == 1) {
+                System.out.println("Boom");
+            }
+        }
+    }
+}
